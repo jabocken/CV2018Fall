54e5fddb5c7d735eb8ac56ca8be486f6c08e8b57
Joshua A. Bockenek <jabocken@vt.edu>
2018-12-07 00:38:07 -0500

    Added proper five-fold cross-validation for CNN
    Works a bit odder than expected as it reuses the cached CNN rather than
    retraining for each training set, but whatever.

--- identifyComponents.m
+++ identifyComponents.m
@@ -8,7 +8,7 @@ data = [H L];
 run matconvnet-1.0-beta25/matlab/vl_setupnn % fix this for your setup
 % path needed for MatConvNet's cnn_train
 addpath('matconvnet-1.0-beta25/examples'); % fix this for your setup; an environment variable might be good, not sure 
-% cnn_train wants images as a 4D array; resizing to NxN for ease of use
+% cnn_train wants images as a 4D array; resizing them to NxN for ease of use
 N = 32;
 C2 = zeros(N,N,3,numel(C), 'single');
 for i = 1:numel(C)
@@ -18,7 +18,32 @@ end
 C2 = C2 - mean(C2, 4); % Want all the means
 
 classCount = size(LMap, 1);
-indices = ones(size(L));
-indices(1:uint32(numel(L) / 5)) = 2; % first 1/5th is validation set
-[net, stats] = train(C2, L, classCount, indices);
-% cnn_train generates figure showing error rates
+
+n = numel(L);
+sectional = uint32(n / 5);
+last = sectional;
+first = 1;
+i = 1;
+count = 0;
+err = 0;
+lastIteration = false;
+while lastIteration == false
+    % want to terminate after last is n, but still execute that time
+    if last == n
+        lastIteration = true;
+    end
+
+    indices = ones(size(L));
+    indices(first:last) = 2; % validation indices
+    [~, stats] = train(C2, L, classCount, indices);
+    err = err + stats.val(end).top1err;
+    count = count + 1;
+
+    first = last + 1;
+    last = last + sectional;
+    if last > n
+        last = n;
+    end
+end
+
+avgAccuracy = 1 - err / count %#ok<NOPTS>
